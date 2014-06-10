ba0c1bf1e11cbed9e069a39fa08fcc597acc4e87
versis <versis791@gmail.com>
2014-06-10 02:49:53 +0200

    Poprawki bledow

--- src/svd.py
+++ src/svd.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import data
-
-
+import data as dt
 def findNumber(S, level):
     size = len(S)
     
@@ -16,23 +14,25 @@ def findNumber(S, level):
             return i
     return size
     
-def getSVDRec(dataArray, level = 0.7):
-
-    usersNo = len(np.unique(dataArray[:,0]))
-    moviesNo = len(np.unique(dataArray[:,1]))
+def getReccomendations(dataArray, level = 0.7):
+    
+    #dataArray = dataBase = dt.getIt('../data/u1.base')[:6]
+        
     dataNo = len(dataArray)
     
-    dataMatrix = np.zeros(shape=(usersNo, moviesNo))
-
+    dataMatrix = np.zeros(shape=(dt.getUsersNo(), dt.getMoviesNo()))
+    
     for i in range(dataNo):
         row = dataArray[i,0]-1
         col = dataArray[i,1]-1
         value = dataArray[i,2]
         dataMatrix[row, col] = value
         
-    movieMeans = dataMatrix.sum(0)/(dataMatrix != 0).sum(0)
+    movieMeans = dataMatrix.sum(0) / (dataMatrix != 0).sum(0)
+    movieMeans[np.isnan(movieMeans)] = 0
     
-    for i in range(moviesNo):
+    
+    for i in range(dt.getMoviesNo()):
         dataMatrix[:,i][(dataMatrix[:,i] == 0)] = movieMeans[i]
     
     U, S1, V = np.linalg.svd(dataMatrix, full_matrices=False)
@@ -42,7 +42,3 @@ def getSVDRec(dataArray, level = 0.7):
     S2 = np.diag(S1)
     
     return np.dot(U, np.dot(S2, V))
-
-
-dataArray = data.getAll()
-a = getSVDRec(dataArray)
