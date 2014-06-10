b15d90eed8ccde29bd0e5e7c75f423cc09ba74b1
versis <versis791@gmail.com>
2014-06-10 02:21:54 +0200

    wiecej danych

--- src/data.py
+++ src/data.py
@@ -2,8 +2,27 @@
 
 import numpy as np
 
+def getIt(fileName):
+    data = np.genfromtxt(fileName, dtype = int)
+    return data[:,:3]
+    
 
-def readIt():
-    fileName = '../data/u.data'
-    data = np.genfromtxt(fileName)
-    return data
+# cale dane
+def getAll():
+    return getIt('../data/u.data')
+
+
+# zbior uczacy nr 1 (80%)
+def getBase1():
+    return getIt('../data/u1.base')
+# zbior testowy nr 1 (20%)
+def getTest1():
+    return getIt('../data/u1.test')
+
+
+# zbior uczacy nr 2(80%)
+def getBase2():
+    return getIt('../data/u2.base')
+# zbior testowy nr 2 (20%)
+def getTest2():
+    return getIt('../data/u2.test')
