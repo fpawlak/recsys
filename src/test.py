078bacce12bcaa69dc1527d2058e5d8a06fd83e8
versis <versis791@gmail.com>
2014-06-10 02:49:03 +0200

    Taki wstepny plik z testem SVD dla danych 80/20 (co innego niz omawialismy)

---
+++ src/test.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import svd
+import data as dt
+
+
+dataBase = dt.getBase1()
+dataTest = dt.getTest1()
+
+recs = svd.getReccomendations(dataBase)
+
+badScores = 0
+
+size = len(dataTest)
+
+for i in range(size):
+    predictedValue = recs[dataTest[i,0], dataTest[i,1]]
+    value = dataTest[i,2]
+    if abs(value - predictedValue) >= 0.5:
+        badScores += 1
+
+quality = (size - badScores)*1.0 / size
+print quality
