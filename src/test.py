762375ee3031752b57a5ff6ab35d64ac8720d5f7
versis <versis791@gmail.com>
2014-06-22 19:15:15 +0200

    Przywrocenie podizalu testow dla svd

--- src/test.py
+++ src/test.py
@@ -7,7 +7,39 @@ import data as dt
 import numpy as np
 
 
-def svdQuality(data, testSize = 10, sampleSize = 6, level = 0.45):
+def svdQualitySingle(data, testSize = 4, level = 0.45):
+#==============================================================================
+# Sprawdza jakosc SVD wyciagajac pojedyncze elementy.
+# Zwraca procentowa trafnosc predykcji.
+#==============================================================================
+    size = len(data)
+    dataMatrix = dt.toDataMatrix(data)
+    testData = random.sample(xrange(1, size), testSize) # losujemy testSize probek, ktore poddamy ocenie
+    
+    badScores = 0 # ustawiamy licznik zlej predykcji na zero
+    
+    for i in range(testSize):
+        
+        # pobieramy dane o badanej probce
+        row = data[testData[i], 0] - 1
+        col = data[testData[i], 1] - 1
+        value = data[testData[i], 2]
+        
+        dataMatrix[row, col] = 0 # ustawiamy probke na zero (tak jakby pierowtnie nie bylo tam wyniku)
+        predictData = svd.getRecommendations(dataMatrix, level) # liczymy predykcje
+        predictedValue = predictData[row, col] # pobieramy wartosc predykcji dla tej probki
+
+        # sprawdzamy roznice
+        if abs(value - predictedValue) >= 0.5:
+            badScores += 1
+
+        dataMatrix[row, col] = value # przypisujemy pierwotna wartosc
+    
+    quality = float(testSize - badScores) / testSize
+    return quality
+
+
+def svdQualityGroups(data, testSize = 10, sampleSize = 6, level = 0.45):
 #==============================================================================
 #     Sprawdza jakosc SVD wyciagajac pojedyncze elementy (grupami).
 #     Zwraca procentowa trafnosc predykcji.
@@ -63,6 +95,7 @@ def slopeOneQuality(data, testSize):
 #     Sprawdza jakosc SlopeOne wyciagajac pojedyncze elementy. 
 #     Zwraca procentowa trafnosc predykcji.
 #==============================================================================
+
     s = s1.SlopeOne()
     s.setData(data)
     s.computeDiffs()
