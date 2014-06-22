503d5ddcb540df1bae37ea17c600456e2374be01
Filip Pawlak <filip.pawlak@gmail.com>
2014-06-22 20:34:12 +0200

    magiczna funkcja

--- src/s1.py
+++ src/s1.py
@@ -14,6 +14,14 @@ class SlopeOne(object):
         self.noOfUsers = np.zeros(shape=(self.moviesNo+1, self.moviesNo+1))
         self.avgDifference = np.zeros(shape=(self.moviesNo+1, self.moviesNo+1))        
 
+    def compPred(self, numerator, denominator):
+        try:
+            prediction = numerator/denominator
+        except:
+            prediction = 0
+
+        return prediction
+        
     def setData(self, data):
         (height, _) = data.shape
         self.d = {}
@@ -76,10 +84,7 @@ class SlopeOne(object):
             numerator += nOU * (rating + diff)
             denominator += nOU
 
-        if denominator >= 1:
-            return numerator/denominator
-        else:
-            return 0
+        return (numerator, denominator)
 
     # "Usuwa" ocene z danych i ja przewiduje.
     def remove_and_predict(self, user, movie):
@@ -115,16 +120,132 @@ class SlopeOne(object):
                 numerator += nOU * (rating + diff)
                 denominator += nOU
 
-        if denominator >= 1:
-            return numerator/denominator
-        else:
-            return 0            
+        return self.compPred(numerator, denominator)
+
+    # origMatrix to pierwotna macierz
+    # dataMatrix to macierz po przepuszczeniu przez fillMatrix
+    # zwraca liste par postaci (i, j, nowa predykcja)
+    def modMatrix(self, user, movie, origMatrix, dataMatrix):
+        user_index = user - 1
+        movie_index = movie - 1
+
+        rating = origMatrix[user_index, movie_index]
+
+        predictions = []
+
+        new_nOU = np.zeros(shape=self.moviesNo+1)
+        new_avgD = np.zeros(shape=self.moviesNo+1)
+
+        user_ratings = d[user]
+        (height, _) = user_ratings.shape
+
+        js = []
+
+        im = movie # tak dla wygody
+        
+        for k in range(0, height):
+            jm = user_ratings[k, 0]
+            if jm == im:
+                continue
+            js.append(jm-1)
+            tD = self.totalDifference[im, jm] - rating
+            new_nOU[jm] = self.noOfUsers[im, jm] - 1
+            if new_nOU[jm] >= 1:
+                new_avgD[jm] = tD/new_nOU[jm]
+
+        # zmieniamy wiersz uzytkownika user:
+
+        for l in range(0, self.moviesNo):
+            if l == movie_index:
+                numerator = 0
+                denominator = 0
+
+                for k in range(0, height):
+                    from_movie = user_ratings[k, 0]
+                    if from_movie == movie:
+                        continue
+                    from_rating = user_ratings[k, 1]
+
+                    nOU = new_nOU[from_movie]
+                    diff = new_avgD[from_movie]
+
+                    numerator += nOU * (from_rating + diff)
+                    denominator += nOU
+
+                prediction = self.compPred(numerator, denominator)
+                predictions.append(user_index, movie_index, prediction)
+
+                
+            else:
+                if origMatrix[user_index, l] > 0:
+                    continue
+
+                lm = l + 1
+
+                numerator = num[user_index, l]
+                numerator -= self.noOfUsers[lm, im]*(rating + self.avgDifference[lm, im])
+                denominator = den[user_index, l]
+                denominator -= self.noOfUsers[lm, im]
+
+                prediction = self.compPred(numerator, denominator)
+                predictions.append(user_index, l, prediction)
+
+        # zmieniamy pozostale wiersze:
+
+        for k in range(0, self.usersNo):
+            if k == user_index:
+                continue
+
+            if origMatrix[k, movie_index] > 0:
+                i_rating = origMatrix[k, movie_index]
+                
+                for j in js:
+                    if origMatrix[k, j] > 0:
+                        continue
+                        
+                    jm = j + 1
+
+                    numerator = num[k, j]
+                    numerator -= self.noOfUsers[jm, im]*(i_rating + self.avgDifference[jm, im])
+                    numerator += new_nOU[jm]*(i_rating + (-new_avgD[jm]))
+                    denominator = den[k, j] - 1
+
+                    prediction = self.compPred(numerator, denominator)
+                    predictions.append(k, j, prediction)
+                        
+            else:
+                numerator = num[k, movie_index]
+                denominator = den[k, movie_index]
+                
+                for j in js:
+                    if origMatrix[k, j] == 0:
+                        continue
+
+                    j_rating = origMatrix[k, j]                        
+                    jm = j + 1
+                        
+                    numerator -= self.noOfUsers[im, jm]*(j_rating + self.avgDifference[im, jm])
+                    numerator += new_nOU[jm]*(j_rating + new_avgD[jm])
+                    denominator -= 1
+
+                    
+                prediction = self.compPred(numerator, denominator)
+                predictions.append(k, movie_index, prediction)
+                
 
     def fillMatrix(self, dataMatrix):
+        self.num = np.zeros(shape=(self.usersNo, self.moviesNo))        
+        self.den = np.zeros(shape=(self.usersNo, self.moviesNo))
+        
         for i in range(0, self.usersNo):
             for j in range(0, self.moviesNo):
                 if dataMatrix[i, j] == 0:
-                    dataMatrix[i, j] = self.predict(i+1, j+1)
+                    (numerator, denominator) = self.predict(i+1, j+1)
+
+                    self.num[i, j] = numerator
+                    self.den[i, j] = denominator
+                    
+                    dataMatrix[i, j] = self.compPred(numerator, denominator)
 
 # dane = dt.getBase1()
 # s = SlopeOne()
