a2ace2ae75c75c3f37bdd51ce63ea8d1d8063792
versis <versis791@gmail.com>
2014-06-22 16:39:45 +0200

    usuniecie zbednych rzeczy z testow

--- src/test.py
+++ src/test.py
@@ -2,27 +2,12 @@
 
 import svd
 import data as dt
-import numpy as np
 
+data = dt.getAll()
 
-dataBase = dt.getBase1()
-dataTest = dt.getTest1()
-
-recs = svd.getRecommendations(dt.toDataMatrix(dataBase), level = 0.41)
-
+#recs = svd.getRecommendations(dt.toDataMatrix(dataBase), level = 0.41)
 #recs = np.loadtxt('txt/u1base.txt')
 #recs = svd.getRecommendations(np.loadtxt('txt/u1base.txt'))
 
-badScores = 0
 
-size = len(dataTest)
-print size
-
-for i in range(size):
-    predictedValue = recs[dataTest[i,0], dataTest[i,1]]
-    value = dataTest[i,2]
-    if abs(value - predictedValue) >= 0.5:
-        badScores += 1
-
-quality = float(size - badScores) / size
-print quality
+print svd.testQuality(data)
